38ef45f3239e3712d4833c9beec3940236c2fd05
João <povoajoao6@gmail.com>
2021-10-01 14:13:00 -0300

    Salem Project page

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+//Areas
+let navbar = document.querySelector('.navbar');
+let loginForm = document.querySelector('.login-form');
+
+
+//Clickables
+let menuIcon = document.querySelector('#mobile');
+let loginArea = document.querySelector('#login-btn');
+
+
+window.onscroll = () => {
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+loginArea.onclick = () => {
+    loginForm.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+    loginForm.classList.remove('active');
+
+}
+
+//Slider Pagination
+
+var swiper = new Swiper(".modelos-slider", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 3500,
+        disableOnInteraction: false,
+    },
+    loop: true,
+});
